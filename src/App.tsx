bdc8da84a70c8fa0a47b7d8265d5dc8a5551d22a
Marco <marcoaurelio_7@outlook.com>
2021-06-24 12:02:47 -0300

    :rocket::rocket::rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 import { AuthContextProvider } from './contexts/AuthContext'
+import { AdminRoom } from './pages/AdminRoom';
 import { Room } from './pages/Room';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter >
